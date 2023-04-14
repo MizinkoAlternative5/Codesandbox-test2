624d4d9d41d78f3dd669ead3fdd90b9d1478f02b
MizinkoAlternative5 <67320419+MizinkoAlternative5@users.noreply.github.com>
2023-04-14 13:08:09 +0900

    途中経過

--- src/index.js
+++ src/index.js
@@ -1,7 +1,65 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompleteList(inputText);
+};
+
+const createIncompleteList = (text) => {
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = text;
+  div.appendChild(li);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    // div以下を初期化
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+    addTarget.appendChild(li);
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      deleteFromCompleteList(backButton.parentNode);
+    });
+    addTarget.appendChild(backButton);
+
+    const completeList = document.getElementById("complete-list");
+    completeList.appendChild(addTarget);
+  });
+  div.appendChild(completeButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+  div.appendChild(deleteButton);
+
+  const incompleteList = document.getElementById("incomplete-list");
+  incompleteList.appendChild(div);
+
+  console.log(div);
+  console.log(li);
+};
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const deleteFromCompleteList = (target) => {
+  document.getElementById("complete-list").removeChild(target);
 };
 
 document
